d6bff29081449c7dfc1f48b6f99e73199e081872
raedion <u548026g@ecs.osaka-u.ac.jp>
2020-11-27 13:52:05 +0900

    Add github-api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use github_rs::client::{Executor, Github};
+use serde_json::Value;
+
 fn main() {
-    println!("Hello, world!");
+    let client = Github::new("API TOKEN").unwrap();
+    let me = client.get()
+        .user()
+        .execute::<Value>();
+
+    match me {
+        Ok((headers, status, json)) => {
+            println!("{:#?}", headers);
+            println!("status: {}", status);
+            if let Some(json) = json{
+                println!("JSON: {}", json);
+            }
+        },
+        Err(e) => println!("error: {}", e)
+    }
 }
